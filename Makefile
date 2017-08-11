e50ecacd34101da726cad484186952fb2c568b77
Lars Ingebrigtsen <larsi@gnus.org>
2017-08-11 13:40:25 +0200

    Attempt to get less laggy clicks

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 copy:
 	rsync -av ../emerald/ajax-loader.gif ../emerald/emerald.* ../emerald/hammer* ../emerald/jquery* ../emerald/index.html www/
-	rsync -av ../emerald/*.ttf www/
+	rsync -av ../emerald/*.ttf ../emerald/fastclick.min.js www/
 	sed 's/only screen and (max-width: 760px)/only screen and (max-width: 6000px)/' < www/emerald.css > a.css && mv a.css www/emerald.css
 	echo "isMobile = true;" >> www/emerald.js
 	echo "phoneGap = true;" >> www/emerald.js
--- www/emerald.js
+++ www/emerald.js
@@ -710,7 +710,7 @@ function rearrangeForMobile() {
   $("body").append($menu);
   if (phoneGap)
     $("table.actions").find("tbody").append($("<tr><td id='share'>Share</td></tr>"));
-  $("table.actions").find("tbody").append($("<tr><td id='close-menu'>Close</td></tr>"));
+  $("table.actions").find("tbody").append($("<tr><td id='close-menu'>Close</td></tr><tr><td id='menu-spacer'>&nbsp;</td></tr>"));
 
   $.map([creators, cover], function(elem) {
     var tr = document.createElement("tr");
@@ -854,6 +854,11 @@ function prepareStart() {
     if (window.innerWidth < 760)
       isMobile = true;
     if (isMobile) {
+      if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+	  FastClick.attach(document.body);
+	}, false);
+      }
       rearrangeForMobile();
       var mc = new Hammer($("body")[0]);
       mc.on("swipeleft", function() {
